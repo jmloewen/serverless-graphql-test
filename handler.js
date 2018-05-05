77bce2a0e7ab25099bde02f73b1a35a498787c2a
unknown <jmloewen@sfu.ca>
2018-05-04 18:09:40 -0700

    pushing demo from serverless website here https://serverless.com/blog/make-serverless-graphql-api-using-lambda-dynamodb/

---
+++ handler.js
@@ -0,0 +1,97 @@
+/* handler.js */
+const {
+  graphql,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLNonNull
+} = require('graphql')
+
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+// add to handler.js
+const promisify = foo => new Promise((resolve, reject) => {
+  foo((error, result) => {
+    if(error) {
+      reject(error)
+    } else {
+      resolve(result)
+    }
+  })
+})
+
+// This method just inserts the user's first name into the greeting message.
+const getGreeting = firstName => promisify(callback =>
+  dynamoDb.get({
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: { firstName },
+  }, callback))
+  .then(result => {
+    if(!result.Item) {
+      return firstName
+    }
+    return result.Item.nickname
+  })
+  .then(name => `Hello, ${name}.`)
+  
+  
+const changeNickname = (firstName, nickname) => promisify(callback =>
+  dynamoDb.update({
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: { firstName },
+    UpdateExpression: 'SET nickname = :nickname',
+    ExpressionAttributeValues: {
+      ':nickname': nickname
+    }
+  }, callback))
+  .then(() => nickname)
+
+
+// Here we declare the schema and resolvers for the query
+// alter schema
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    /* unchanged */
+  }),
+  mutation: new GraphQLObjectType({
+    name: 'RootMutationType', // an arbitrary name
+    fields: {
+      changeNickname: {
+        args: {
+          // we need the user's first name as well as a preferred nickname
+          firstName: { name: 'firstName', type: new GraphQLNonNull(GraphQLString) },
+          nickname: { name: 'nickname', type: new GraphQLNonNull(GraphQLString) }
+        },
+        type: GraphQLString,
+        // update the nickname
+        resolve: (parent, args) => changeNickname(args.firstName, args.nickname)
+      }
+    }
+  })
+})
+
+// We want to make a GET request with ?query=<graphql query>
+// The event properties are specific to AWS. Other providers will differ.
+module.exports.query = (event, context, callback) => graphql(schema, event.queryStringParameters.query)
+  .then(
+    result => callback(null, {statusCode: 200, body: JSON.stringify(result)}),
+    err => callback(err)
+  )
+
+
+//The "Hello World" func
+module.exports.hello = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
